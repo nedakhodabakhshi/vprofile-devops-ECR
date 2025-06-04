c8befeb1ab87495a9862a1f264adef37f8053c21
Neda Khodabakhshi <khodabakhshi.neda@gmail.com>
2025-06-04 14:07:10 +0200

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+echo "Installing Docker..."
 sudo apt update -y
 sudo apt install docker.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo usermod -aG docker ubuntu
