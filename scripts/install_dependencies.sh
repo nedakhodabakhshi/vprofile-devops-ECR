a8a44e0b811a99e4a8cc6e85198b9eb4fc5ac081
nedakhodabakhshi <khodabakhshi.neda@gmail.com>
2025-06-05 10:00:21 +0200

    Initial commit

--- scripts/install_dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Installing Docker..."
-sudo apt update -y
-sudo apt install docker.io -y
-sudo systemctl start docker
-sudo systemctl enable docker
--- scripts/stop_containers.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Stopping any running containers..."
-docker stop $(docker ps -q) || true
-docker rm $(docker ps -a -q) || true
